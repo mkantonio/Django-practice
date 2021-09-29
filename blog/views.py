90565d79a6d8f626ffa412d680491ca15bbf0f4e
Marco Antonio <mkchoque@gmail.com>
2021-09-29 10:25:27 -0400

    pagination and CBV Django

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
@@ -32,3 +33,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
